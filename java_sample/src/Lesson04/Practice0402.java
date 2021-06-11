d1c2534c5b143ba012c2c0e0721cb04f5b8d601e
Szyoo <37945314+Szyoo@users.noreply.github.com>
2021-06-11 17:23:24 +0900

    Add files via upload

---
+++ java_sample/src/Lesson04/Practice0402.java
@@ -0,0 +1,8 @@
+package Lesson04;
+
+public class Practice0402 {
+    public static void main(String[] args) {
+        int num = 30;
+        System.out.println("変数numの値は" + num + "です");
+    }
+}
---
+++ java_sample/src/lesson03/Sample0303.java
@@ -0,0 +1,7 @@
+package Lesson03;
+
+public class Sample0303 {
+    public static void main(String[] args) {
+        System.out.println("ようこそJavaへ!");
+    }
+}
