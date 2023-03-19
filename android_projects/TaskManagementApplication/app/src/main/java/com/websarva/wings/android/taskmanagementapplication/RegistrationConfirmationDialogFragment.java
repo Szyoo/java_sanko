1360d82ab445fdd7454b0add8ff90390efa4fb16
Szyoo <losiner.y@gmail.com>
2023-03-19 23:17:23 +0900

    作业上传

--- android_projects/TaskManagementApplication/app/src/main/java/com/websarva/wings/android/taskmanagementapplication/RegistrationConfirmationDialogFragment.java
+++ android_projects/TaskManagementApplication/app/src/main/java/com/websarva/wings/android/taskmanagementapplication/RegistrationConfirmationDialogFragment.java
@@ -27,7 +27,11 @@ public class RegistrationConfirmationDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         //ここに処理を書く
         //AlertDialog dialog = null;の部分は開発の際に削除してダイアログの生成処理に書き換えること
-        AlertDialog dialog = null;
+        AlertDialog.Builder builder=new AlertDialog.Builder((getActivity()));
+        builder.setMessage(R.string.task_registration_cancel_dialog_msg);
+        builder.setPositiveButton(R.string.btn_registration,new DialogButtonClickListener());
+        builder.setNegativeButton(R.string.btn_cancel,new DialogButtonClickListener());
+        AlertDialog dialog = builder.create();
         return dialog;
     }
 
@@ -35,6 +39,37 @@ public class RegistrationConfirmationDialogFragment extends DialogFragment {
         @Override
         public void onClick(DialogInterface dialog, int which) {
             //ここに処理を書く
+            switch (which) {
+                case DialogInterface.BUTTON_POSITIVE:
+                    Bundle bundle = getArguments();
+                    String taskDescription = bundle.getString("taskDescription");
+                    long finishDateTime = bundle.getLong("finishDateTime");
+                    int taskStatus = bundle.getInt("taskStatus");
+                    int checkedTaskNotificationId = bundle.getInt("checkedTaskNotificationId");
+                    int checkedTaskGroupId = bundle.getInt("checkedTaskGroupId");
+                    int checkedTaskColorId = bundle.getInt("checkedTaskColorId");
+
+                    TaskDatabaseHelper helper = new TaskDatabaseHelper(getActivity());
+                    SQLiteDatabase db = helper.getWritableDatabase();
+
+                    String sql = "INSERT INTO task " +
+                            "(name, finish_at, status, notification, task_group, color, created_at) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+                    SQLiteStatement stmt = db.compileStatement(sql);
+                    stmt.bindString(1, taskDescription);
+                    stmt.bindLong(2, finishDateTime);
+                    stmt.bindLong(3, taskStatus);
+                    stmt.bindLong(4, checkedTaskNotificationId);
+                    stmt.bindLong(5, checkedTaskGroupId);
+                    stmt.bindLong(6, checkedTaskColorId);
+
+                    long today = new Date(System.currentTimeMillis()).getTime();
+                    stmt.bindLong(7, today);
+                    stmt.executeInsert();
+                    getActivity().finish();
+                case DialogInterface.BUTTON_NEGATIVE:
+                    break;
+            }
         }
     }
 }
--- android_projects/TaskManagementApplication/app/src/main/java/com/websarva/wings/android/taskmanagementapplication/TaskDetailsFragment.java
+++ android_projects/TaskManagementApplication/app/src/main/java/com/websarva/wings/android/taskmanagementapplication/TaskDetailsFragment.java
@@ -23,7 +23,90 @@ public class TaskDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_task_details, container, false);
 
         //ここに処理を書く
+        Bundle bundle = getArguments();
+        int taskId = bundle.getInt("_id");
 
+        TaskDatabaseHelper helper = new TaskDatabaseHelper(getActivity());
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String sql = "SELECT * FROM task WHERE _id = " + taskId;
+        Cursor cursor = db.rawQuery(sql, null);
+        while (cursor.moveToNext()) {
+            int taskNameIdx = cursor.getColumnIndex("name");
+            String taskName = cursor.getString(taskNameIdx);
+            TextView tvTaskName = view.findViewById(R.id.tvTaskDetailDescription);
+            tvTaskName.setText(taskName);
+
+            int taskFinishDateTimeIdx = cursor.getColumnIndex("finish_at");
+            Long taskFinishDateTime = cursor.getLong(taskFinishDateTimeIdx);
+            Date finishDateTime = new Date(taskFinishDateTime);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            String finishDate = dateFormat.format(finishDateTime);
+            String finishTime = timeFormat.format(finishDateTime);
+            TextView tvFinishDate = view.findViewById(R.id.tvTaskDetailFinishDate);
+            tvFinishDate.setText(finishDate);
+            TextView tvFinishTime = view.findViewById(R.id.tvTaskDetailFinishTime);
+            tvFinishTime.setText(finishTime);
+
+            int taskStatusIdx = cursor.getColumnIndex("status");
+            int taskStatus = cursor.getInt(taskStatusIdx);
+
+            String[] taskStatusArray = getResources().getStringArray(R.array.sp_task_status);
+            String taskStatusStr = taskStatusArray[taskStatus];
+            TextView tvTaskStatus = view.findViewById(R.id.tvTaskDetailStatus);
+            tvTaskStatus.setText(taskStatusStr);
+            int taskNotificationIdx = cursor.getColumnIndex("notification");
+            int taskNotification = cursor.getInt(taskNotificationIdx);
+            TextView tvTaskNotification = view.findViewById(R.id.tvTaskDetailNotify);
+            switch (taskNotification) {
+                case 0:
+                    tvTaskNotification.setText(R.string.rb_notify);
+                    break;
+                case 1:
+                    tvTaskNotification.setText(R.string.rb_not_notify);
+                    break;
+                default:
+                    break;
+            }
+            int taskGroupIdx = cursor.getColumnIndex("task_group");
+            int taskGroup = cursor.getInt(taskGroupIdx);
+            TextView tvTaskGroup = view.findViewById(R.id.tvTaskDetailGroup);
+            switch (taskGroup) {
+                case 0:
+                    tvTaskGroup.setText(R.string.important);
+                    break;
+                case 1:
+                    tvTaskGroup.setText(R.string.emergency);
+                    break;
+                case 2:
+                    tvTaskGroup.setText(R.string.note);
+                    break;
+                case 3:
+                    tvTaskGroup.setText(R.string.not_set);
+                    break;
+                default:
+                    break;
+            }
+            int taskColorIdx = cursor.getColumnIndex("color");
+            int taskColor = cursor.getInt(taskColorIdx);
+            TextView tvTaskColor = view.findViewById(R.id.tvTaskDetailColor);
+            switch (taskColor) {
+                case 0:
+                    tvTaskColor.setText(R.string.rb_blue);
+                    break;
+                case 1:
+                    tvTaskColor.setText(R.string.rb_red);
+                    break;
+                case 2:
+                    tvTaskColor.setText(R.string.rb_green);
+                    break;
+                case 3:
+                    tvTaskColor.setText(R.string.not_set);
+                    break;
+                default:
+                    break;
+            }
+        }
         return view;
     }
 
